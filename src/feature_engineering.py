70e8dcc3059458db2750471e218273e5d60e4d3f
hamidreza-s-salehi <92396224+hamidreza-s-salehi@users.noreply.github.com>
2024-07-05 16:20:26 +0330

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,33 +1,39 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
+from .data_preparation import encode_categorical_features 
 
 TOP_N_FEATURES = 50
 
 def select_features(df_encoded):
     X = df_encoded.drop(columns='Target', axis=1)
     y = df_encoded['Target']
+
+    X, _ = encode_categorical_features(X)
+
     gini_model = DecisionTreeClassifier()
     gini_model.fit(X, y)
     feature_importances = pd.Series(gini_model.feature_importances_, index=X.columns)
     top_features_tree = feature_importances.nlargest(TOP_N_FEATURES)
-    top_selected_feature = pd.Series(list(set(top_features_tree.index)))
-    top_selected_feature = top_selected_feature.append(pd.Series(["Target"]))
-    df_final = df_encoded[top_selected_feature]
+    top_selected_feature = pd.concat([pd.Series(top_features_tree.index), pd.Series(["Target"])])
+    df_final = df_encoded.loc[:, top_selected_feature]
     return df_final
 
 def normalize_data(df):
+    numeric_columns = df.select_dtypes(include=[float, int]).columns
     scaler = MinMaxScaler()
-    std_df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
-    return std_df
+    std_df = pd.DataFrame(scaler.fit_transform(df[numeric_columns]), columns=numeric_columns)
+    df.loc[:, numeric_columns] = std_df
+    return df
 
 if __name__ == "__main__":
-    from data_preparation import load_data, clean_data, encode_data
+    from .data_preparation import load_data, clean_data, encode_data, encode_categorical_features
 
     radiomics, morphological = load_data()
     dataset_merged = clean_data(radiomics, morphological)
     df_encoded, target_encoder = encode_data(dataset_merged)
+    df_encoded, label_encoders = encode_categorical_features(df_encoded)
     df_final = select_features(df_encoded)
     df_final_normalized = normalize_data(df_final)
     print(df_final_normalized.head())
