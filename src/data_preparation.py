00c252349a976401c148a05450f8416e618ef34b
hamidreza-s-salehi <92396224+hamidreza-s-salehi@users.noreply.github.com>
2024-07-05 15:16:05 +0330

    Update data_preparation.py

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,13 +1,27 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def load_data():
-    dataset_radiomics = pd.read_excel('data/OpenBTAI_RADIOMICS.xlsx')
-    dataset_morphological = pd.read_excel('data/OpenBTAI_MORPHOLOGICAL_MEASUREMENTS.xlsx')
-    return dataset_radiomics, dataset_morphological
+    radiomics = pd.read_excel('data/OpenBTAI_RADIOMICS.xlsx')
+    morphological = pd.read_excel('data/OpenBTAI_MORPHOLOGICAL_MEASUREMENTS.xlsx')
+    return radiomics, morphological
 
-def prepare_data(datasets):
-    dataset_radiomics, dataset_morphological = datasets
-    dataset_radiomics.drop(["Timepoint", "Label", "Lesion", "Segment"], axis=1, inplace=True)
-    dataset_morphological = dataset_morphological[["PATIENT", "Primary Tumor"]]
-    dataset_merged = pd.merge(dataset_morphological, dataset_radiomics, on='PATIENT')
+def clean_data(radiomics, morphological):
+    radiomics.drop(["Timepoint", "Label", "Lesion", "Segment"], axis=1, inplace=True)
+    morphological = morphological[["PATIENT", "Primary Tumor"]]
+    dataset_merged = pd.merge(morphological, radiomics, on='PATIENT')
     return dataset_merged
+
+def encode_data(dataset_merged):
+    df_encoded = dataset_merged.copy()
+    df_encoded.rename(columns={"Primary Tumor": "Target"}, inplace=True)
+    df_encoded.drop("PATIENT", axis=1, inplace=True)
+    target_encoder = LabelEncoder()
+    df_encoded["Target"] = target_encoder.fit_transform(df_encoded["Target"])
+    return df_encoded, target_encoder
+
+if __name__ == "__main__":
+    radiomics, morphological = load_data()
+    dataset_merged = clean_data(radiomics, morphological)
+    df_encoded, target_encoder = encode_data(dataset_merged)
+    print(df_encoded.head())
